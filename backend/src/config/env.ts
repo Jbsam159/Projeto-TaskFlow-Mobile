4811b251bcfda158585bc6bb8889f91bfa4eea91
JoaoVictor <jbsam159@gmail.com>
2026-02-27 12:31:54 -0300

    chore: commit das issues 1 e 2 da fase do backend

---
+++ backend/src/config/env.ts
@@ -0,0 +1,21 @@
+import dotenv from "dotenv"
+import { dot } from "node:test/reporters"
+
+dotenv.config()
+
+function getEnvVariable(key: string): string {
+
+  const value = process.env[key]
+
+  if(!value){
+    throw new Error(`❌ Faltando Variável de Ambiente: ${key}`)
+  }
+
+  return value
+
+}
+
+export const env = {
+  PORT: Number(getEnvVariable("PORT")),
+  NODE_ENV: getEnvVariable("NODE_ENV"),
+}
