8a2bfb8b0ffed8a1302dab4ffa22cfc9e2209f57
JoaoVictor <jbsam159@gmail.com>
2026-03-02 11:28:54 -0300

    completando issues 3 e 4 da fase 1 - Iniciando setup do backend

---
+++ backend/src/app.ts
@@ -0,0 +1,12 @@
+// Importando lib para criar o servidor
+import express from "express"
+
+import { router } from "./routes/index.js"
+
+// Criando instância do app
+const app = express()
+
+app.use(express.json())
+app.use(router)
+
+export {app}
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,17 +1,7 @@
-// Importando lib para criar o servidor
-import express from "express"
+import {app} from "./app.js"
+
 // Importando variáveis env
 import {env} from "../src/config/env.js"
-
-// Criando instância do app
-const app = express()
-
-app.use(express.json())
-
-// Criando rota de teste
-app.get("/health", (req, res) => {
-  return res.json({ status: 'ok' });
-})
 
 // Escutando a porta
 app.listen(env.PORT, () => {
