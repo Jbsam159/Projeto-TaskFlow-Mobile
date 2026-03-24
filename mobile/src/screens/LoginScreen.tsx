099e4cca7280d2f2a66612c062635c3867affea3
JoaoVictor <jbsam159@gmail.com>
2026-03-24 12:33:36 -0300

    feat: melhorando a LoginScreen e seu UI e UX

--- mobile/src/screens/LoginScreen.tsx
+++ mobile/src/screens/LoginScreen.tsx
@@ -1,11 +1,94 @@
-import { View, Text, Button } from "react-native"
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from "react-native"
+import { useState } from "react"
 
 export default function LoginScreen({ navigation }: any) {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleLogin = () => {
+  // 🔥 VALIDAÇÃO AQUI
+  if (!email || !password) {
+    console.log("Preencha todos os campos")
+    return
+  }
+
+    // futura chamada da API
+    console.log("Email:", email)
+    console.log("Senha:", password)
+  }
+
+
   return (
-    <View>
-      <Text>Login</Text>
-      <Button title="Ir para Home" onPress={() => navigation.navigate("Home")} />
-      <Button title="Criar conta" onPress={() => navigation.navigate("Register")} />
+    <View style={styles.container}>
+      <Text style={styles.title}>Login</Text>
+
+      <TextInput
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+        style={styles.input}
+        keyboardType="email-address"
+        autoCapitalize="none"
+      />
+
+      <TextInput
+        placeholder="Senha"
+        value={password}
+        onChangeText={setPassword}
+        style={styles.input}
+        secureTextEntry
+      />
+
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Entrar</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => navigation.navigate("Register")}>
+        <Text style={styles.link}>Criar conta</Text>
+      </TouchableOpacity>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 20,
+    backgroundColor: "#fff",
+  },
+
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+    textAlign: "center",
+  },
+
+  input: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    padding: 12,
+    marginBottom: 10,
+    borderRadius: 8,
+  },
+
+  button: {
+    backgroundColor: "#007bff",
+    padding: 15,
+    borderRadius: 8,
+    alignItems: "center",
+    marginTop: 10,
+  },
+
+  buttonText: {
+    color: "#fff",
+    fontWeight: "bold",
+  },
+
+  link: {
+    marginTop: 15,
+    textAlign: "center",
+    color: "#007bff",
+  },
+})
